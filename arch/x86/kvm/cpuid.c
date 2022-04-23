0db9381b3cdae7c63a2fc3840580b8e52220100c
aaryaneil <aaryaneil_nimbalkar@cmpe283-vm.us-west1-b.c.aerobic-datum-345615.internal>
2022-04-23 05:18:24 +0000

    assignment 2

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -1439,16 +1439,55 @@ bool kvm_cpuid(struct kvm_vcpu *vcpu, u32 *eax, u32 *ebx,
 }
 EXPORT_SYMBOL_GPL(kvm_cpuid);
 
+uint64_t total_exits=0;
+EXPORT_SYMBOL(total_exits);
+
+u32 exit_per_reason[69]={0};
+EXPORT_SYMBOL(exit_per_reason);
+
 int kvm_emulate_cpuid(struct kvm_vcpu *vcpu)
 {
 	u32 eax, ebx, ecx, edx;
 
 	if (cpuid_fault_enabled(vcpu) && !kvm_require_cpl(vcpu, 0))
 		return 1;
-
+	
 	eax = kvm_rax_read(vcpu);
 	ecx = kvm_rcx_read(vcpu);
+
+	if(eax== 0x4fffffff){
+          eax= total_exits; 
+     }else if(eax==0x4ffffffd){
+
+        if(ecx >=0 && ecx <=69 && ecx !=35 && ecx !=38 && ecx !=42  && ecx != 65){
+            
+                    //check for returning all zeros for exit types not handled in the kvm
+                    if(ecx!=3 && ecx!=4 && ecx!=5 && ecx!=6 && ecx!=11 && ecx!=34 && ecx!=33 && ecx!=51 && ecx<63){
+                    
+                      eax = exit_per_reason[(int)ecx];
+                    }else{
+                    
+                   eax= 0x00000000;
+                   ebx= 0x00000000;
+                   ecx= 0x00000000;
+                   edx= 0x00000000; 
+                    }
+                    
+                     
+                 
+
+ 	}else{   
+ 		// for exit types out of range 0 to 69
+ 	           eax= 0x00000000;
+                   ebx= 0x00000000;
+                   ecx= 0x00000000;
+                   edx= 0xffffffff; 
+ 
+ 	}
+   }
+    else{
 	kvm_cpuid(vcpu, &eax, &ebx, &ecx, &edx, false);
+      }
 	kvm_rax_write(vcpu, eax);
 	kvm_rbx_write(vcpu, ebx);
 	kvm_rcx_write(vcpu, ecx);
